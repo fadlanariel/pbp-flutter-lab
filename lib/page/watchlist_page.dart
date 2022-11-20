8c8f74cb6eb5820ce8d9413d8803d71f004b128e
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2022-11-20 20:56:41 +0700

    Make watchlist title clickable & do bonus task

--- lib/page/watchlist_page.dart
+++ lib/page/watchlist_page.dart
@@ -41,54 +41,54 @@ class _MyWatchlistPageState extends State<MyWatchlistPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Watch List'),
-      ),
-      // Menambahkan drawer menu
-      drawer: Drawer(
-        child: Column(
-          children: [
-            // Menambahkan clickable menu
-            ListTile(
-              title: const Text('counter_7'),
-              onTap: () {
-                // Route menu ke halaman utama
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyHomePage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Tambah Budget'),
-              onTap: () {
-                // Route menu ke halaman form
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyFormPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Data Budget'),
-              onTap: () {
-                // Route menu ke halaman form
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MyDataPage())
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('My Watch List'),
-              onTap: () {
-                // Route menu ke halaman form
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('My Watch List'),
         ),
-      ),
+        // Menambahkan drawer menu
+        drawer: Drawer(
+          child: Column(
+            children: [
+              // Menambahkan clickable menu
+              ListTile(
+                title: const Text('counter_7'),
+                onTap: () {
+                  // Route menu ke halaman utama
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Tambah Budget'),
+                onTap: () {
+                  // Route menu ke halaman form
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyFormPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Data Budget'),
+                onTap: () {
+                  // Route menu ke halaman form
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MyDataPage()));
+                },
+              ),
+              ListTile(
+                title: const Text('My Watch List'),
+                onTap: () {
+                  // Route menu ke halaman form
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
         body: FutureBuilder(
             future: fetchWatchlist(),
             builder: (context, AsyncSnapshot snapshot) {
@@ -100,56 +100,72 @@ class _MyWatchlistPageState extends State<MyWatchlistPage> {
                     children: const [
                       Text(
                         "Tidak ada watchlist :(",
-                        style: TextStyle(
-                            color: Color(0xff59A5D8),
-                            fontSize: 20),
+                        style:
+                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                       ),
                       SizedBox(height: 8),
                     ],
                   );
                 } else {
                   return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index)=> Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => InkWell(
+                      onTap: () {
+                        print("Test");
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 12),
                         padding: const EdgeInsets.all(20.0),
                         decoration: BoxDecoration(
-                            color:Colors.white,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(15.0),
-                            boxShadow: const [
-                              BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 2.0
-                              )
-                            ]
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                            Text(
-                              "${snapshot.data![index].fields.title}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            border: Border.all(
+                                  color: snapshot.data![index].fields.watched
+                                      ? Colors.green
+                                      : Colors.redAccent,
+                                  ),
                             ),
-                            Text(
-                              "Watched : ${snapshot.data![index].fields.watched}",
-                              style: const TextStyle(
-                                fontSize: 15.0,
-                                color: Colors.grey,
+                        child: Card(
+                          elevation: 0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    snapshot.data![index].fields.title,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      color:
+                                          snapshot.data![index].fields.watched
+                                              ? Colors.green
+                                              : Colors.redAccent,
+                                    ),
+                                  ),
+                                  Checkbox(
+                                      value:
+                                          snapshot.data![index].fields.watched,
+                                      onChanged: (bool? value) {
+                                        setState(() {
+                                          snapshot.data![index].fields.watched =
+                                              value!;
+                                        });
+                                      }),
+                                ],
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      )
+                      ),
+                    ),
                   );
                 }
               }
-            }
-        )
-    );
+            }));
   }
 }
